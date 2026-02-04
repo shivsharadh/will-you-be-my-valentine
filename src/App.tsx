0851bf7a814fb4a38cfefe57bcd7d5be81367297
Shiv Sharadh <98868570+shivsharadh@users.noreply.github.com>
2026-02-04 12:26:32 -0600

    Refactor App component and update GIFs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+"use client";
 import { useState } from "react";
 
-// GIFS
+// GIFS YOU CAN CHANGE
 const DEFAULT_GIF =
-  "https://media1.tenor.com/m/yopy9CIqfgUAAAAd/ek-niranjan-brahmi.gif";
+  "https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif";
 
 const YES_GIF =
-  "https://gifdb.com/images/high/gabbar-singh-brahmi-gif-63req7bpd9dl8x07.webp";
+  "https://media.giphy.com/media/l4FGuhL4U2WyjdkaY/giphy.gif";
 
 const NO_GIFS = [
   "https://media.giphy.com/media/ISOckXUybVfQ4/giphy.gif",
@@ -13,77 +14,59 @@ const NO_GIFS = [
   "https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif",
 ];
 
-export default function App()  {
+export default function Page() {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
 
-  const yesButtonSize = noCount * 20 + 16;
-
   const handleNoClick = () => {
     setNoCount((prev) => prev + 1);
-  };
-
-  const getNoButtonText = () => {
-    const phrases = [
-      "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
-      ":((((",
-      "PRETTY PLEASE",
-      "Estoy muerto",
-      "No :(",
-    ];
-
-    return phrases[Math.min(noCount, phrases.length - 1)];
   };
 
   const currentNoGif =
     NO_GIFS[Math.min(noCount - 1, NO_GIFS.length - 1)];
 
   return (
-    <div className="-mt-16 flex h-screen flex-col items-center justify-center">
-      {yesPressed ? (
-        <>
-          <img src={YES_GIF} />
-          <div className="my-4 text-4xl font-bold">
-            WOOOOOO!!! I love you pookie!! ;))
-          </div>
-        </>
-      ) : (
-        <>
-          <img
-            className="h-[200px]"
-            src={noCount === 0 ? DEFAULT_GIF : currentNoGif}
-          />
+    <div className="flex h-screen flex-col items-center justify-center">
+      {/* IMAGE */}
+      <img
+        className="h-[200px]"
+        src={
+          yesPressed
+            ? YES_GIF
+            : noCount === 0
+            ? DEFAULT_GIF
+            : currentNoGif
+        }
+      />
 
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+      {/* TEXT */}
+      {!yesPressed && (
+        <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+      )}
 
-          <div className="flex items-center">
-            <button
-              className="mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700"
-              style={{ fontSize: yesButtonSize }}
-              onClick={() => setYesPressed(true)}
-            >
-              Yes
-            </button>
+      {/* BUTTONS */}
+      {!yesPressed && (
+        <div className="flex gap-4">
+          <button
+            className="rounded bg-green-500 px-4 py-2 font-bold text-white"
+            onClick={() => setYesPressed(true)}
+          >
+            Yes
+          </button>
 
-            <button
-              onClick={handleNoClick}
-              className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
-            >
-              {getNoButtonText()}
-            </button>
-          </div>
-        </>
+          <button
+            className="rounded bg-red-500 px-4 py-2 font-bold text-white"
+            onClick={handleNoClick}
+          >
+            No
+          </button>
+        </div>
+      )}
+
+      {yesPressed && (
+        <div className="mt-4 text-4xl font-bold">
+          WOOOOOO!!! 💖
+        </div>
       )}
     </div>
   );
